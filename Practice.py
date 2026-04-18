a6aa46ab9ef78c3d28393fe1e818e6d2fa156e0a
Akshat <akshatverma4578@gmail.com>
2026-04-18 20:03:59 +0530

    learning hashing in python

--- Practice.py
+++
@@ -1,4 +0,0 @@
-print("hello world")
-a=6
-b=7
-print(a+b)
--- practice_.py
+++ practice_.py
@@ -1,3 +1,9 @@
-a=4
-b=7
-print(a+b)
+n = [1,2,3,4,4,5,5,7,5,9,10]
+m = [10,111,23,2,3,4,5,9,45]
+
+for num in m:
+    count = 0
+    for x in n:
+        if x==num:
+            count+=1
+    print(count, end="")
